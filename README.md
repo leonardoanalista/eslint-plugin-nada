9d0f9543a960ca479df6c04c074d2874facf676c
Leonardo Correa <leonardoanalista@gmail.com>
2019-04-27 22:28:14 +1000

    feat: first commit for this file path rule

---
+++ README.md
@@ -0,0 +1,67 @@
+# eslint-plugin-pathname
+
+Enforces full path, folder and file names to match desired case style
+
+* Quick example:
+```
+// Errors for rule: kebabCase
+myFileFolder/my-file-name.js // file path
+
+// Ok for rule: kebabCase
+my-file-folder/my-file-name.js // file path
+
+```
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-pathname`:
+
+```
+$ npm install eslint-plugin-pathname --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-pathname` globally.
+
+## Usage
+
+Add `pathname` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+  "plugins": [
+    "pathname"
+  ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+  "rules": {
+    "pathname/rule-name": [{"case": "kebabCase"}]
+  }
+}
+```
+
+
+## Supported Rules
+
+* Supported case options are: kebabCase, camelCase and snakeCase.
+
+## Roadmap
+
+* auto fix by renaming the file path to match desired rule
+* maybe Typescript
+
+
+
+
+
---
+++ tests/lib/rules/path-case.js
@@ -0,0 +1,98 @@
+/**
+ * @fileoverview enforces folder and file names to match desired case style
+ * @author path-case
+ */
+
+const { RuleTester } = require('eslint');
+const rule = require('../../../lib/rules/path-case');
+
+const type = 'Program';
+const ruleTester = new RuleTester();
+
+ruleTester.run('path-case', rule, {
+  valid: [
+    {
+      filename: 'my-file-folder/my-file-name.js',
+      code: 'my-file-folder/my-file-name.js // rule: kebabCase',
+      options: [{ case: 'kebabCase' }],
+    },
+    {
+      filename: 'myFileFolder/myFileName.js',
+      code: 'myFileFolder/myFileName.js // rule: camelCase',
+      options: [{ case: 'camelCase' }],
+    },
+    {
+      filename: 'my_file_folder/my_file_name.js',
+      code: 'my_file_folder/my_file_name.js // rule: snakeCase',
+      options: [{ case: 'snakeCase' }],
+    },
+  ],
+
+  invalid: [
+    // kebabCase
+    {
+      filename: 'myFileFolder/my-file-name.js',
+      code: 'myFileFolder/my-file-name.js // rule: kebabCase',
+      options: [{ case: 'kebabCase' }],
+      errors: [
+        {
+          message:
+            "Path filename 'myFileFolder/my-file-name.js' does not match the naming convention 'kebabCase'. Correct is 'my-file-folder/my-file-name.js'",
+          type,
+        },
+      ],
+    },
+    {
+      filename: 'my-file-folder/myFileName.js',
+      code: 'my-file-folder/myFileName.js // rule: kebabCase',
+      options: [{ case: 'kebabCase' }],
+      errors: [
+        {
+          message:
+            "Path filename 'my-file-folder/myFileName.js' does not match the naming convention 'kebabCase'. Correct is 'my-file-folder/my-file-name.js'",
+          type,
+        },
+      ],
+    },
+
+    // camelCase
+    {
+      filename: 'my-file-folder/my-file-name.js',
+      code: 'my-file-folder/my-file-name.js // rule: camelCase',
+      options: [{ case: 'camelCase' }],
+      errors: [
+        {
+          message:
+            "Path filename 'my-file-folder/my-file-name.js' does not match the naming convention 'camelCase'. Correct is 'myFileFolder/myFileName.js'",
+          type,
+        },
+      ],
+    },
+
+    // snakeCase
+    {
+      filename: 'my-file-folder/my-file-name.js',
+      code: 'my-file-folder/my-file-name.js // rule: snakeCase',
+      options: [{ case: 'snakeCase' }],
+      errors: [
+        {
+          message:
+            "Path filename 'my-file-folder/my-file-name.js' does not match the naming convention 'snakeCase'. Correct is 'my_file_folder/my_file_name.js'",
+          type,
+        },
+      ],
+    },
+    {
+      filename: 'my-File-Folder/my-File-Name.js',
+      code: 'my-File-Folder/my-File-Name.js // rule: snakeCase',
+      options: [{ case: 'snakeCase' }],
+      errors: [
+        {
+          message:
+            "Path filename 'my-File-Folder/my-File-Name.js' does not match the naming convention 'snakeCase'. Correct is 'my_file_folder/my_file_name.js'",
+          type,
+        },
+      ],
+    },
+  ],
+});
